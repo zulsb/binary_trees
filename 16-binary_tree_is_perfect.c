c79cb1f12524423baa976805ef6d6d5ccd093572
zulsb <zulsb2093@gmail.com>
2020-03-13 19:06:08 +0000

    Correction function that checks if a binary tree is perfect.

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,23 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_leaves - Function that counts the leaves in a binary tree.
+ * @tree: Pointer to root node.
+ * Return: 1 for leaf.
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (!(tree->right) && !(tree->left))
+		return (1);
+	else
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
+
+/**
  * binary_tree_height - Function that measures the height of a binary tree.
  * @tree: Pointer to node.
  * Return: The height of a node.
@@ -12,6 +29,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t h_right = 0;
 
 	if (tree == NULL)
+		return (0);
+
+	if (!(tree->left) && !(tree->right))
 		return (0);
 
 	h_left = (1 + binary_tree_height(tree->left));
@@ -30,17 +50,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int pow = 1;
+	int leaf = binary_tree_leaves(tree);
+	int height = binary_tree_height(tree);
+
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
+	while (height > 0)
+	{
+		pow *= 2;
+		height -= 1;
+	}
+
+	if (leaf == pow)
 		return (1);
-
-	if (binary_tree_height(tree->left) == 0)
-		return (0);
-
-	if (binary_tree_height(tree->right) != binary_tree_height(tree->left))
-		return (0);
-
-	return (1);
+	return (0);
 }
